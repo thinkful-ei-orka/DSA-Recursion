5a3c2c892e16ff1890f3c9acb3c485545735b2b3
Jonathan Clark <jonny@pop-os.localdomain>
2020-07-13 18:48:13 -0700

    Co-authored-by: Carlo Paredes <c.nick.paredes@gmail.com>

--- anagrams.js
+++ anagrams.js
@@ -1,5 +1,16 @@
-function anagram(str) {
+let storeAnagram = [];
+
+function anagram(pre, str) {
   if (str.length < 2) {
-    return str;
+    console.log(`Pre: ${pre}, Str: ${str}`);
+    storeAnagram.push(pre + str);
+    return pre + str;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    anagram(pre + str.charAt(i), str.substring(0, i) + str.substring(i + 1));
   }
 }
+
+anagram('', 'abc');
+console.log(storeAnagram);
